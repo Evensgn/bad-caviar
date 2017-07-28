0c713b4c96cc3c81e714abe53675f7949b78e6c7
Evensgn <i@evensgn.com>
2017-07-28 17:03:55 +0800

    Generate submit files

--- process.py
+++ process.py
@@ -29,19 +29,40 @@ for text in data_texts:
 	text_pro = " ".join(words)
 	data_texts_pro.append(text_pro)
 
+
+filenames = []
+for i in range(6000):
+	filenames.append('comment_' + str(i))
+alldata_texts = []
+for filename in filenames:
+	alldata_texts.append(str(read_file('data/comment_new/' + filename + '.txt')))
+
+alldata_texts_pro = []
+for text in alldata_texts:
+	text_p = text.replace('\n', '').replace('\r', '')
+	words = []
+	for word in jieba.cut(text_p):
+		if (word not in stopwords) and (not word.isdigit()):
+			words.append(word)
+	text_pro = " ".join(words)
+	alldata_texts_pro.append(text_pro)
+
 import numpy as np
 from numpy import random
 
 data_arr = list(zip(data_texts_pro, data_labels))
 data_arr = random.permutation(data_arr)
 
-train_texts = data_arr[:900, 0]
-train_labels = data_arr[:900, 1].astype(int)
-test_texts = data_arr[900:, 0]
-test_labels = data_arr[900:, 1].astype(int)
+train_texts = data_arr[:, 0]
+train_labels = data_arr[:, 1].astype(int)
 
-print(test_texts)
-print(test_labels)
+test_texts = np.array(alldata_texts_pro)
+
+# test_texts = data_arr[:, 0]
+# test_labels = data_arr[:, 1].astype(int)
+
+# print(test_texts)
+# print(test_labels)
 
 from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -57,9 +78,9 @@ train_x = train_vecs_arr
 
 from sklearn import svm, metrics
 
-# clf = svm.SVR()
-# clf = svm.SVC(gamma = 1, probability = True)
-clf = svm.SVC(kernel = 'linear', probability = True)
+# clf = svm.SVR(gamma = 1)
+clf = svm.SVC(gamma = 1, probability = True)
+# clf = svm.SVC(kernel = 'linear', probability = True)
 
 clf.fit(train_x, train_labels)
 
@@ -73,11 +94,18 @@ prediction = clf.predict_proba(test_x)
 # print(prediction[:, 1])
 
 # test_auc = metrics.roc_auc_score(test_labels, prediction)
-test_auc = metrics.roc_auc_score(test_labels, prediction[:, 1])
-print(test_auc)
+# test_auc = metrics.roc_auc_score(test_labels, prediction[:, 1])
+# print(test_auc)
 
 '''
 print("Classification report for classifier %s:\n%s\n"
 	  % (clf, metrics.classification_report(test_labels, prediction)))
 print("Confusion matrix:\n%s" % metrics.confusion_matrix(test_labels, prediction))
 '''
+
+headers = ['id', 'pred']
+rows = list(zip(filenames, prediction[:, 1]))
+with open('ans.csv', 'w') as f:
+	f_csv = csv.writer(f)
+	f_csv.writerow(headers)
+	f_csv.writerows(rows)
