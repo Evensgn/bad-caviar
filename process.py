392f118fbfdc7cf3d6f93da6c260ff70bf06d4b2
Evensgn <i@evensgn.com>
2017-07-28 15:45:50 +0800

    Modify stopwords and process.py

--- process.py
+++ process.py
@@ -21,7 +21,7 @@ import jieba
 
 data_texts_pro = []
 for text in data_texts:
-	text_p = text.replace(' ', '').replace('\n', '').replace('\r', '')
+	text_p = text.replace('\n', '').replace('\r', '')
 	words = []
 	for word in jieba.cut(text_p):
 		if (word not in stopwords) and (not word.isdigit()):
@@ -29,12 +29,20 @@ for text in data_texts:
 	text_pro = " ".join(words)
 	data_texts_pro.append(text_pro)
 
-train_texts = data_texts_pro[:800]
-train_labels = data_labels[:800]
-test_texts = data_texts_pro[800:]
-test_labels = data_labels[800:]
-
 import numpy as np
+from numpy import random
+
+data_arr = list(zip(data_texts_pro, data_labels))
+data_arr = random.permutation(data_arr)
+
+train_texts = data_arr[:900, 0]
+train_labels = data_arr[:900, 1].astype(int)
+test_texts = data_arr[900:, 0]
+test_labels = data_arr[900:, 1].astype(int)
+
+print(test_texts)
+print(test_labels)
+
 from sklearn import feature_extraction
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import PCA
@@ -43,22 +51,33 @@ vectorizer = TfidfVectorizer()
 train_texts = np.array(train_texts)
 train_vecs = vectorizer.fit_transform(train_texts)
 train_vecs_arr = train_vecs.toarray()
-pca = PCA(n_components = 100)
-train_x = pca.fit_transform(train_vecs_arr)
+pca = PCA(n_components = 500)
+# train_x = pca.fit_transform(train_vecs_arr)
+train_x = train_vecs_arr
 
 from sklearn import svm, metrics
 
-clf = svm.SVC(gamma = 1)
-# clf = svm.SVC(kernel = 'linear')
+# clf = svm.SVR()
+# clf = svm.SVC(gamma = 1, probability = True)
+clf = svm.SVC(kernel = 'linear', probability = True)
 
 clf.fit(train_x, train_labels)
 
 test_vecs = vectorizer.transform(test_texts)
 test_vecs_arr = test_vecs.toarray()
-test_x = pca.transform(test_vecs_arr)
+# test_x = pca.transform(test_vecs_arr)
+test_x = test_vecs_arr
 
-prediction = clf.predict(test_x)
+# prediction = clf.predict(test_x)
+prediction = clf.predict_proba(test_x)
+# print(prediction[:, 1])
 
+# test_auc = metrics.roc_auc_score(test_labels, prediction)
+test_auc = metrics.roc_auc_score(test_labels, prediction[:, 1])
+print(test_auc)
+
+'''
 print("Classification report for classifier %s:\n%s\n"
 	  % (clf, metrics.classification_report(test_labels, prediction)))
 print("Confusion matrix:\n%s" % metrics.confusion_matrix(test_labels, prediction))
+'''
